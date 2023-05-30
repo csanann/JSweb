a3ed345e2058d05e0f09c81cc6195fc2c0cebade
csanann <csanann@gmail.com>
2023-05-30 16:13:48 +0100

    Add example-1 folder

---
+++ example-1/add.js
@@ -0,0 +1,7 @@
+// file: example-1/add.js
+
+function add(a, b) {
+    return a + b;
+}
+
+module.exports = add;
---
+++ example-1/multiply.js
@@ -0,0 +1,7 @@
+// file: example-1/multiply.js
+
+function multiply(a, b) {
+    return a * b;
+}
+
+module.exports = multiply;
